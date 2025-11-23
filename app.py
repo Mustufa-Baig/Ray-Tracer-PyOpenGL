130eec0a09818bf7acd182c1587a27e7bb793957
unknown <mustufabaigwastaken@gmail.com>
2025-11-23 23:28:14 +0530

    Added Quads

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class App:
 		glUseProgram(self.shader)
 
 		self.triangle=Triangle()
+		self.quad=Quad()
 		self.mainloop()
 
 	def createShader(self,vertex_path,fragment_path):
@@ -40,16 +41,57 @@ class App:
 			glClear(GL_COLOR_BUFFER_BIT)
 
 			glUseProgram(self.shader)
-			glBindVertexArray(self.triangle.vao)
-			glDrawArrays(GL_TRIANGLES,0,self.triangle.vertex_count)
+
+			#glBindVertexArray(self.triangle.vao)
+			#glDrawArrays(GL_TRIANGLES,0,self.triangle.vertex_count)
+			
+			glBindVertexArray(self.quad.vao)
+			glDrawArrays(GL_TRIANGLES, 0, self.quad.vertex_count)
 
 			pygame.display.flip()
 			self.clock.tick(60)
 
 		self.triangle.destroy()
+		self.quad.destroy()
+
 		glDeleteProgram(self.shader)
 		pygame.quit()
 
+
+class Quad:
+    def __init__(self):
+        # Two triangles forming a quad
+        self.vertices = numpy.array([
+            # pos               # color
+            -0.5, -0.5, 0.0,     1.0, 1.0, 0.0,
+             0.5, -0.5, 0.0,     0.0, 1.0, 1.0,
+             0.5,  0.5, 0.0,     1.0, 0.0, 1.0,
+
+            -0.5, -0.5, 0.0,     1.0, 1.0, 0.0,
+             0.5,  0.5, 0.0,     1.0, 0.0, 1.0,
+            -0.5,  0.5, 0.0,     0.0, 1.0, 0.0,
+        ], dtype=numpy.float32)
+
+        self.vertex_count = 6
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        # Position
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
+        # Color
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
 
 class Triangle:
 	def __init__(self):
