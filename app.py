644c7839851c956fa92d0221fd30b1b8ec1555e7
unknown <mustufabaigwastaken@gmail.com>
2026-03-18 22:50:48 +0530

    added multi-frame sample accumilation

--- app.py
+++ app.py
@@ -17,6 +17,20 @@ class App:
 
 		#self.triangle=Triangle()
 		self.quad=Quad()
+
+		self.frame_count = 0
+		self.fbos = glGenFramebuffers(2)
+		self.textures = glGenTextures(2)
+
+		for i in range(2):
+			glBindFramebuffer(GL_FRAMEBUFFER, self.fbos[i])
+			glBindTexture(GL_TEXTURE_2D, self.textures[i])
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 700, 400, 0, GL_RGBA, GL_FLOAT, None)
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, self.textures[i], 0)
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0)
 		self.mainloop()
 
 	def createShader(self,vertex_path,fragment_path):
@@ -35,34 +49,54 @@ class App:
 
 	def mainloop(self):
 		run=True
+		
+		res_addr =glGetUniformLocation(self.shader,"u_resolution")
+		frame_addr = glGetUniformLocation(self.shader, "u_frameCount")
+		accum_tex_addr = glGetUniformLocation(self.shader, "u_accumTexture")
+
 		while run:
 			for event in pygame.event.get():
 				if event.type==pygame.QUIT:
 					run=False
 
-			glClear(GL_COLOR_BUFFER_BIT)
+			read_idx = self.frame_count % 2
+			write_idx = (self.frame_count + 1) % 2
+
+			
+			glBindFramebuffer(GL_FRAMEBUFFER, self.fbos[write_idx])
 
 			glUseProgram(self.shader)
+			
+			glUniform2f(res_addr, WIDTH, HEIGHT)
+			glUniform1f(frame_addr, float(self.frame_count))
 
-			res_addr=glGetUniformLocation(self.shader,"u_resolution")
-			time_addr=glGetUniformLocation(self.shader,"u_time")
+			glActiveTexture(GL_TEXTURE0)
+			glBindTexture(GL_TEXTURE_2D, self.textures[read_idx])
+			glUniform1i(accum_tex_addr, 0)
 
-			current_time=pygame.time.get_ticks()/1000.0
-			glUniform2f(res_addr,WIDTH,HEIGHT)
-			glUniform1f(time_addr,current_time)
-			#glBindVertexArray(self.triangle.vao)
-			#glDrawArrays(GL_TRIANGLES,0,self.triangle.vertex_count)
+			
+			glBindVertexArray(self.quad.vao)
+			glDrawArrays(GL_TRIANGLES, 0, self.quad.vertex_count)
+
+			
+			glBindFramebuffer(GL_READ_FRAMEBUFFER, self.fbos[write_idx])
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)
+			glBlitFramebuffer(0, 0, 700, 400, 0, 0, 700, 400, GL_COLOR_BUFFER_BIT, GL_NEAREST)
+
+
 			
 			glBindVertexArray(self.quad.vao)
 			glDrawArrays(GL_TRIANGLES, 0, self.quad.vertex_count)
 
 			pygame.display.flip()
+			self.frame_count+=1
 			self.clock.tick(60)
 
 		#self.triangle.destroy()
 		self.quad.destroy()
-
 		glDeleteProgram(self.shader)
+		glDeleteFramebuffers(2, self.fbos)
+		glDeleteTextures(2, self.textures)
 		pygame.quit()
 
 
